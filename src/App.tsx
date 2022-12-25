be2d917072cd4a800f1b67d2d54b3d7df428ff3a
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-25 02:55:11 -0300

    Instala styled components, e cria arquivo de tema das cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
-    <h1>My finances 2.0</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>My finances 2.0</h1>
+    </ThemeProvider>
   )
 }

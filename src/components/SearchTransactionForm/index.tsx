631decd46f570efd08b5e9ec0be659acc2aa6f37
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-29 06:42:50 -0300

    Finaliza estilizacao do componente Search

--- src/components/SearchTransactionForm/index.tsx
+++ src/components/SearchTransactionForm/index.tsx
@@ -1,12 +1,13 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { SearchContainer } from "./styles";
 
 export function SearchTransactionForm() {
   return (
-    <form>
+    <SearchContainer>
       <input type="text" placeholder="Buscar transações" />
       <button type="submit">
         <MagnifyingGlass size={22} />
       </button>
-    </form>
+    </SearchContainer>
   )
 }
--- src/components/SearchTransactionForm/styles.ts
+++ src/components/SearchTransactionForm/styles.ts
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+export const SearchContainer = styled.form`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  input {
+    flex: 1;
+    padding: 1rem;
+    border-radius: 6px;
+    background-color: ${({theme}) => theme["gray-900"]};
+    color: ${({theme}) => theme["gray-300"]};
+    border: none;
+    line-height: 22.4px;
+  }
+
+  button {
+    padding: 0.875rem 1rem;
+    border-radius: 6px;
+    border: none;
+    border: 1px solid ${({theme}) => theme["green-300"]};
+    color: ${({theme}) => theme["green-300"]};
+    background-color: transparent;
+    font-weight: bold;
+    cursor: pointer;
+
+    :hover {
+      border-color: ${({theme}) => theme["green-500"]};
+      color: ${({theme}) => theme.white};
+      background-color: ${({theme}) => theme["green-500"]};
+      transition: background-color 0.2s, color 0.2s, border-color 0.2s;
+    }
+  }
+`

d6c58c939aa6aff82bba1959d87c4a870ad2f7d1
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-30 04:24:43 -0300

    Importa da biblioteca Radix componente primitivo de Modal para criar uma nova transacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/igniteLogo.svg'
+
 import { HeaderContainer, HeaderContent } from './styles'
 
 export function Header() {
@@ -9,7 +12,23 @@ export function Header() {
           <img src={logoImg} alt="" />
           <p>my finance<span>$</span></p>
         </h1>
-        <button>Nova transação</button>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button>Nova transação</button>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+
+          </Dialog.Portal>
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )

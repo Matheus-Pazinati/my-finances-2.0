df4202bec9baaece84e69a061128405a959b70fd
Matheus Pazinati <matheuspazinati@gmail.com>
2023-01-04 05:05:23 -0300

    Calcula os ganhos, gastos e o total com base nas transacoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,23 +1,44 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../context/TransactionsContext";
 import { SummaryCard } from "./components/SummaryCard";
 import { EarningsIcon, ExpenseIcon, SummaryContainer, TotalIcon } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce((accumulator, transaction) => {
+    if (transaction.type === "income") {
+      accumulator.income += transaction.price
+    } else {
+      accumulator.expense += transaction.price
+    }
+    accumulator.total = accumulator.income - accumulator.expense
+
+    return accumulator
+  },
+    {
+      income: 0,
+      expense: 0,
+      total: 0
+    }
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard
         title="Entradas"
         Icon={EarningsIcon}
-        amount={17.500}
+        amount={summary.income}
       />
       <SummaryCard
         title="Saídas"
         Icon={ExpenseIcon}
-        amount={17.500}
+        amount={summary.expense}
       />
       <SummaryCard
         title="Total"
         Icon={TotalIcon}
-        amount={17.500}
+        amount={summary.total}
       />
     </SummaryContainer>
   )
